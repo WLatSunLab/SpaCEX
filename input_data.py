095a9a9d60d5cd78d7eebd02e25bbd237454ae73
Win <121435520+zipging@users.noreply.github.com>
2023-08-06 17:07:50 +0800

    Update input_data.py

--- input_data.py
+++ input_data.py
@@ -1,8 +1,8 @@
 import numpy as np
 from keras.datasets import mnist
 import torch
-import tensorflow as tf
 from torch.utils.data import Dataset
+import tensorflow as tf
 
 
 def load_mnist(num):
@@ -13,22 +13,22 @@ def load_mnist(num):
     x = np.divide(x, 255.)
     x = x[:num]
     y = y[:num]
-    num = int(num)
     print('MNIST samples', x.shape)
     return x, y
 
-
 def load_cifar10(num):
+
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
     x = np.concatenate((x_train, x_test))
     y = np.concatenate((y_train, y_test)).astype(np.int32)
     x = x.astype(np.float32)
     x = np.divide(x, 255.)
+    x = np.transpose(x, (0, 3, 1, 2))
     x = x[:num]
     y = y[:num]
-    num = int(num)
     print('Cifar10 samples', x.shape)
     return x, y
+
 
 
 class MnistDataset(Dataset):
@@ -44,7 +44,6 @@ class MnistDataset(Dataset):
         return (torch.from_numpy(np.array(self.x[idx]))).unsqueeze(0), torch.from_numpy(
             np.array(self.y[idx])), torch.from_numpy(np.array(idx))
 
-
 class Cifar10Dataset(Dataset):
 
     def __init__(self, num):
@@ -55,8 +54,10 @@ class Cifar10Dataset(Dataset):
         return self.x.shape[0]
 
     def __getitem__(self, idx):
-        return (torch.from_numpy(np.array(self.x[idx]))), torch.from_numpy(
+        return torch.from_numpy(np.array(self.x[idx])), torch.from_numpy(
             np.array(self.y[idx])), torch.from_numpy(np.array(idx))
+
+
 
 
 
