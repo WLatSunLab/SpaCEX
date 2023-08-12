f712c85dd757e6602f762d360f750609080f9bb9
soya <118828920+soya-yaya@users.noreply.github.com>
2023-08-12 23:42:04 +0800

    Update driver.py

--- src/main/driver.py
+++ src/main/driver.py
@@ -1,5 +1,7 @@
 from CAE_encoder import CAE
 from MAE_encoder import MAE
+from VGG_encoder import VGG
+from SwinTranformer_encoder import SwinTransformer
 
 
 '''
@@ -54,6 +56,20 @@ def model(dataset, encoder, config):
                 conv4_outplanes=config['conv4_outplanes'],
                 hidden_size=config['hidden_size'],
                 p=config['p']
+            )
+        if encoder == 'SwinTransformer':
+            model = SwinTransformer(
+                img_size=config['img_size'],
+                patch_size=config['patch_size'],
+                in_channels=config['in_channels'],
+                embed_dim=config['embed_dim'],
+                window_size=config['window_size'],
+                mlp_ratio=config['mlp_ratio'],
+                drop_rate=config['drop_rate'],
+                attn_drop_rate=config['attn_drop_rate'],
+                n_swin_blocks=config['n_swin_blocks'],
+                n_attn_heads=config['n_attn_heads'],
+                n_z=config['n_z']
             )
     if dataset =='Cifar10':
         if encoder == 'CAE':
