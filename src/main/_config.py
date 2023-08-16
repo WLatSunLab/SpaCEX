d14adf3bb24e9e2d5bf1c4be111bab19133a1851
Win <121435520+zipging@users.noreply.github.com>
2023-08-16 14:40:17 +0800

    Update _config.py

--- src/main/_config.py
+++ src/main/_config.py
@@ -41,7 +41,7 @@ class Config:
                     'num_heads': 4,  # multi-head
                     'dim_head': 4,  # head dim in multi-head attention
                     'decoder_embed_dim': 16,  # positional embedding dim
-                    'mlp_ratio': 5,  # mlp_ratio*hidden dim in transformer
+                    'mlp_ratio': 5,  # ratio between the hidden dimension and the embedded dimension
                     'norm_pix_loss': False,  # wheather to normalize
                     'alpha': 0.8,  # alpha in DEC
                     'n_clusters': 10, # number of category, MNIST is 10
@@ -54,7 +54,6 @@ class Config:
                     'lr': 0.001,  # learning rate in pre-training
                     'n_epochs': 100,  # epoch in pre-training
                     'tol': 0.001,  # optimize tolerance in DEC
-                    'update_interval': 1,  # interval in DEC
                     'num_classes': 10,  # number of category, MNIST is 10
                     'conv1_outplanes': 64,  # initial convolution
                     'conv2_outplanes': 128,
@@ -63,52 +62,50 @@ class Config:
                     'hidden_size':512,  # hidden dim
                     'p': 0.5,  # rate of dropout
                     'alpha': 1.0,  # alpha in DEC
-                    'interval': 1  # interbal
+                    'interval': 1  # interval in DEC
                 }
                 'SwinTransformer':{
-                    'model': 'SwinTransformer',  # 做模型标记，后续用作聚类判断
-                    'batch_size': 256,  # 预训练时的batch_size
-                    'lr': 0.001,  # 预训练时的学习率
-                    'alpha': 1.0,  # DEC时的alpha
-                    'n_epochs': 100,  # 预选连的epoch数
-                    'tol': 0.001,  # DEC时优化容忍度
-                    'update_interval': 1,  # DEC时的训练间隔
-                    'num_classes': 10,  # 在MNIST上
-                    'img_size:28, # MNIST图像大小
-                    'patch_size':2, # 输入图像分割成小块（patch）的大小
-                    'n_channels':1, # 输入图像的通道数
-                    'embed_dim':48, # 嵌入维度
-                    'window_size':7, # 局部注意力窗口的大小
-                    'mlp_ratio':4, #  MLP（多层感知机）部分中隐藏层的维度与嵌入维度之间的比率
-                    'drop_rate':0., # Dropout的概率
-                    'attn_drop_rate':0., # 注意力分数（权重）的Dropout概率
-                    'n_swin_blocks':(2, 2), # 每个层级的Swin Transformer块的数量
-                    'n_attn_heads':(2, 4), # 每个层级的注意力头的数量
+                    'model': 'SwinTransformer',  # used for clustering judgement
+                    'batch_size': 256,  # batch size in pre-training
+                    'lr': 0.001,  # learning rate in pre-training
+                    'n_epochs': 100,  # epoch in pre-training
+                    'tol': 0.001,  # optimize tolerance in DEC
+                    'num_classes': 10,  # number of category, MNIST is 10
+                    'img_size:28, # image size
+                    'patch_size':2, # patch size
+                    'n_channels':1, # channel of image
+                    'embed_dim':48, # embedding dim
+                    'window_size':7, # window attention size
+                    'mlp_ratio':4, #  ratio between the hidden dimension and the embedded dimension
+                    'drop_rate':0., # rate of dropout
+                    'attn_drop_rate':0., # rate of dropout in attention
+                    'n_swin_blocks':(2, 2), # number of swin tranformer in each layer
+                    'n_attn_heads':(2, 4), # number of attention head in each layer
+                    'alpha': 1.0,  # alpha in DEC
                     'n_z':96
                 }   
             },
             'Cifar10': {
                 'CAE': {
-                    'model': 'CAE',  # 做模型标记，后续用作聚类判断
-                    'in_channels': 3,  # 在彩色图像如Cifar上Channel为3
-                    'batch_size': 256,  # 预训练时的batch_size
-                    'lr': 0.001,  # 预训练时的学习率
-                    'alpha': 1.0,  # DEC时的alpha
-                    'n_epochs': 100,  # 预选连的epoch数
-                    'tol': 0.001,  # DEC时优化容忍度
-                    'update_interval': 1,  # DEC时的训练间隔
-                    'num_classes': 10,  # 在MNIST上
-                    'basic_num': 2,  # 决定basicblock上的卷积次数，至少为1
-                    'conv1_outplanes': 32,  # 初始卷积参数
+                    'model': 'CAE',  # used for clustering judgement
+                    'in_channels': 3,  # channel of image, Cifar10 is 3
+                    'batch_size': 256,  # batch size in pre-training
+                    'lr': 0.001,  # learning rate in pre-training
+                    'n_epochs': 100,  # dpoch in pre-training
+                    'tol': 0.001,  # oprimize tolerance in DEC
+                    'num_classes': 10,  # number of category, Cifar10 is 10
+                    'basic_num': 2,  # number of covolutions in basicblokc
+                    'conv1_outplanes': 32,  # initial convolution
                     'bolck1_outplanes': 64,
                     'bolck2_outplanes': 128,
                     'bolck3_outplanes': 256,
                     'bolck4_outplanes': 512,
-                    'layers_num': 4,  # layers的层数，至少为1
-                    'maxpool_dr': 1,  # 0表示不用maxpooling降维
-                    'pool_bool': 0,  # 0表示不pooling，1表示avg，2表示max
-                    'n_init': 20,  # kmeans的初始参数
-                    'interval': 1  # DEC时的训练间隔
+                    'layers_num': 4,  # number of basicblock
+                    'maxpool_dr': 1,  # wheather to maxpooling
+                    'pool_bool': 0,  # 0 denote w/o pooling，1 denote avg，2 denote max
+                    'n_init': 20,  # kmeans
+                    'alpha': 1.0,  # alpha in DEC
+                    'interval': 1  # interval in DEC
                 }
             }
         }
