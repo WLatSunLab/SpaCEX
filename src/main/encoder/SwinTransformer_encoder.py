79a1785fe6c16603c10915edb0ae2b088581fd33
Win <121435520+zipging@users.noreply.github.com>
2023-08-16 14:01:32 +0800

    Update SwinTransformer_encoder.py

--- src/main/encoder/SwinTransformer_encoder.py
+++ src/main/encoder/SwinTransformer_encoder.py
@@ -20,27 +20,12 @@ batchsize = 256
 lr = 0.001
 n_input = 784
 
-'''SwinTransformer_encoder
+'''
+SwinTransformer_encoder
 For MNIST data sets
 '''
 
-'''Example
-import VIT
-from input_data import MnistDataset
 
-cuda = torch.cuda.is_available()
-print("use cuda: {}".format(cuda))
-device = torch.device("cuda" if cuda else "cpu")
-dataset = MnistDataset()
-model = SwinTransformer()
-model.to(device)
-model.pretrain(dataset)
-data = dataset.x
-y = dataset.y
-data = torch.Tensor(data).to(device)
-data=data.unsqueeze(1)
-x_bar, hidden = model(data)
-'''
 
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
@@ -63,9 +48,9 @@ class Mlp(nn.Module):
 
 class PatchEmbed(nn.Module):
     """
-    将输入图片进行分区，将每4*4个区域视为一个patch, 将其元素投射到channel维度上
-    使用 conv 操作同时进行分区和线性映射
-    该操作对应论文架构中的PatchPartition和stage1的LinearEmbedding
+    Partition the input image, treat every 4*4 areas as a patch, and project its elements onto the channel dimension.
+    Use conv operations for both partitioning and linear mapping.
+    This operation corresponds to the PatchPartition in the paper schema and the LinearEmbedding of stage1.
     """
 
     def __init__(self, patch_size=4, in_channels=3, embed_dim=512, patch_norm=True):
@@ -88,25 +73,26 @@ class PatchEmbed(nn.Module):
 
 class PatchMerging(nn.Module):
     """
-    首先将相邻的2*2个patch在channel上合并
-    然后将得到的channel维度 4C 映射为 2C
-    input shape: [batch_size, n_channels, h, w]
-    output shape: [batch_size, 2*n_channels, h/2, w/2]
+    The adjacent 2*2 patches are merged on the channel.
+    The resulting channel dimension 4C is then mapped to 2C.
+    input shape: [batch_size, n_channels, h, w].
+    output shape: [batch_size, 2*n_channels, h/2, w/2].
     """
 
     def __init__(self, in_channels):
         super(PatchMerging, self).__init__()
         self.norm = nn.LayerNorm(4 * in_channels)
-        # 使用1*1 conv来进行reduction
+        
+        # Use 1*1 conv for reduction
         self.reduction = nn.Conv2d(in_channels=4 * in_channels, out_channels=2 * in_channels, kernel_size=1, bias=False)
 
     def forward(self, x):
-        # b, c, h, w = x.size()  # 分别为batch_size, n_channels, height_size, width_size
-        x_0 = x[:, :, 0::2, 0::2]  # 宽高从0开始每两个元素取一个，等价于取每一个2*2块的左上角
-        x_1 = x[:, :, 1::2, 0::2]  # 高从1开始每两个元素取一个，等价于取每一个2*2块的左下角
-        x_2 = x[:, :, 0::2, 1::2]  # 宽从1开始每两个元素取一个，等价于取每一个2*2块的右上角
-        x_3 = x[:, :, 1::2, 1::2]  # 宽高从1开始每两个元素取一个，等价于取每一个2*2块的右下角
-        x = torch.cat([x_0, x_1, x_2, x_3], dim=1)  # 在channel维度上将所有的块cat起来
+        # b, c, h, w = x.size()  # batch_size, n_channels, height_size, width_size
+        x_0 = x[:, :, 0::2, 0::2]  
+        x_1 = x[:, :, 1::2, 0::2] 
+        x_2 = x[:, :, 0::2, 1::2]  
+        x_3 = x[:, :, 1::2, 1::2] 
+        x = torch.cat([x_0, x_1, x_2, x_3], dim=1)
         x = x.permute(0, 2, 3, 1)
         x = self.norm(x)
         x = x.permute(0, 3, 1, 2)
@@ -158,7 +144,7 @@ def get_relative_position_index(window_size):
 
 def get_mask(img_size, window_size, shift_size):
     """
-    根据total_size, window_size和shift_size获取mask矩阵
+    Get the mask matrix from total_size, window_size and shift_size
     """
     img_mask = torch.zeros((1, 1, img_size, img_size))  # 1 H W 1
     h_slices = (slice(0, -window_size),
@@ -167,39 +153,26 @@ def get_mask(img_size, window_size, shift_size):
     w_slices = (slice(0, -window_size),
                 slice(-window_size, -shift_size),
                 slice(-shift_size, None))
-    cnt = 0  # cnt代表当前窗口的序号，即把在原图中属不同窗口的各个区域通过序号给区分开来
+    cnt = 0  
     for h in h_slices:
         for w in w_slices:
             img_mask[:, :, h, w] = cnt
             cnt += 1
-    # img_mask[0, 0, h, w] = [
-    # tensor([[0., 0., 0., 0., 1., 1., 2., 2.],
-    #         [0., 0., 0., 0., 1., 1., 2., 2.],
-    #         [0., 0., 0., 0., 1., 1., 2., 2.],
-    #         [0., 0., 0., 0., 1., 1., 2., 2.],
-    #         [3., 3., 3., 3., 4., 4., 5., 5.],
-    #         [3., 3., 3., 3., 4., 4., 5., 5.],
-    #         [6., 6., 6., 6., 7., 7., 8., 8.],
-    #         [6., 6., 6., 6., 7., 7., 8., 8.]])
+    mask_windows = window_partition(img_mask, window_size)
+    mask_windows = mask_windows.view(-1, window_size * window_size)
 
-    mask_windows = window_partition(img_mask, window_size)  # 把整个img_mask划分为各个窗口, nW, window_size, window_size, 1
-    mask_windows = mask_windows.view(-1, window_size * window_size)  # 把每个窗口的值flatten成向量 nW, window_size*window_size
-    # mask_windows[0, :] tensor([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
-    # mask_windows[1, :] tensor([1., 1., 2., 2., 1., 1., 2., 2., 1., 1., 2., 2., 1., 1., 2., 2.])
-    # mask_windows[2, :] tensor([3., 3., 3., 3., 3., 3., 3., 3., 6., 6., 6., 6., 6., 6., 6., 6.])
-    # mask_windows[3, :] tensor([4., 4., 5., 5., 4., 4., 5., 5., 7., 7., 8., 8., 7., 7., 8., 8.])
     # shape: [n_window, window_size**2, window_size**2]
-    attn_mask = mask_windows.unsqueeze(1) - mask_windows.unsqueeze(2)  # 计算每个窗口的mask矩阵, 来自不同窗口的元素的值相减以后非0
-    attn_mask = attn_mask.masked_fill(attn_mask != 0, float(-100.0)).masked_fill(attn_mask == 0, float(0.0))  # 把非0元素改为-100.
+    attn_mask = mask_windows.unsqueeze(1) - mask_windows.unsqueeze(2)
+    attn_mask = attn_mask.masked_fill(attn_mask != 0, float(-100.0)).masked_fill(attn_mask == 0, float(0.0))
 
     return attn_mask
 
 
 class WindowAttention(nn.Module):
     """
-    在一个window内做attention
-    首先根据给定的window_size获取相对位置编码矩阵
-    然后将给定的window所有元素flatten之后做multi head attention
+    Pay attention in a window.
+    First get the relative position encoding matrix based on the given window_size.
+    Then flatten all the elements of the given window and do multi head attention.
     input shape: [num_windows*batch_size,n_channels, window_size, window_size]
     output shape: [num_windows*batch_size,n_channels, window_size, window_size]
     """
@@ -210,20 +183,12 @@ class WindowAttention(nn.Module):
         self.n_heads = n_heads
         self.scale = (n_channels // n_heads) ** -0.5
 
-        # 为每个attention head初始化相位位置编码表, 每个head的表大小为 (2*window_size-1) * (2*window_size-1)
-        # 由于相位位置编码index计算过程中，最大的index为2*(window_size-1)*(2*window_size-1)+2*(window_size-1)
-        # = 2*(window_size-1)*2*window_size = (2 * window_size -1) * (2 * window_size - 1) - 1
-        # 然后只要求相对位置编码index矩阵，然后根据index从对应head的相对位置编码表中取相对位置编码即可
         self.relative_position_bias_table = nn.Parameter(
             torch.zeros((2 * window_size - 1) * (2 * window_size - 1), n_heads)
         )
-        # 使用正态分布值初始化相对位置编码
         trunc_normal_(self.relative_position_bias_table, std=.02)
-        # 获取相对位置编码index矩阵 [window_size**2, window_size**2]
         relative_position_index = get_relative_position_index(window_size)
         self.register_buffer('relative_position_index', relative_position_index)
-        # 同时求Q、K、V, 在channel维度上做attention, 对应文章中的
-        # all query patches within a window share the same key set
         self.qkv = nn.Linear(n_channels, 3 * n_channels)
         self.attention_drop = nn.Dropout(attention_drop)
         self.proj = nn.Linear(n_channels, n_channels)
@@ -232,10 +197,9 @@ class WindowAttention(nn.Module):
 
     def forward(self, x, mask=None):
         b_, n_c, window_size, _ = x.size()
-        # 首先进行维度转换
         # [n_w*batch_sz, n_c, window_sz, window_sz] => [n_w*batch_sz, window_sz, window_sz, n_c]
         x = x.permute(0, 2, 3, 1)
-        # 求q k v
+        # caculate q k v
         qkv = self.qkv(x)  # [n_w*batch_sz, window_sz, window_sz, 3 * n_c]
         qkv = qkv.reshape(b_, window_size * window_size, n_c // self.n_heads, self.n_heads, 3)
         q = qkv[:, :, :, :, 0].permute(0, 3, 1, 2)  # [b_, n_heads, window_sz * window_sz, n_c // n_heads]
@@ -243,14 +207,16 @@ class WindowAttention(nn.Module):
         v = qkv[:, :, :, :, 2].permute(0, 3, 1, 2)  # [b_, n_heads, window_sz * window_sz, n_c // n_heads]
         q *= self.scale  # 对q进行缩放
         att_matrix = q @ k.transpose(-2, -1)  # [b_, n_heads, window_sz * window_sz, window_sz * window_sz]
-        # 根据relative_position_index取出relative_position_encode
+
         # [window_size**2 * window_size**2, n_heads]
         relative_position_encode = self.relative_position_bias_table[self.relative_position_index.view(-1)]
         relative_position_encode = relative_position_encode.view(self.window_size ** 2, self.window_size ** 2, -1)
         relative_position_encode = relative_position_encode.permute(2, 0, 1)  # [n_heads, window_size**2, window_size**2]
-        # 把相对位置编码加到attention矩阵中
+        
+        # add relative position encoding to the attention matrix
         att_matrix = att_matrix + relative_position_encode.unsqueeze(0)
-        # 对attention矩阵进行mask
+        
+        # mask attention matrix
         if mask is not None:
             # mask shape: [n_window, window_size**2, window_size**2] => [n_window, 1, window_size**2, window_size**2]
             n_w = mask.size()[0]
@@ -270,7 +236,7 @@ class WindowAttention(nn.Module):
 
 class SwinTransformerBlock(nn.Module):
     """
-    swin transformer块，支持shift和非shift
+    swin transformer block, support shift and non-shift.
     input shape: [batch_size, n_c, n_h, n_w]
     """
 
@@ -290,7 +256,7 @@ class SwinTransformerBlock(nn.Module):
         self.norm_1 = nn.LayerNorm(n_channels)
         self.mlp = Mlp(in_features=n_channels, hidden_features=int(mlp_ratio * n_channels))
 
-        # 计算mask矩阵
+        # caculate mask matrix
         if self.shift_size > 0:
             # shape: [n_window, window_size**2, window_size**2]
             attn_mask = get_mask(img_size, window_size, shift_size)
@@ -305,6 +271,7 @@ class SwinTransformerBlock(nn.Module):
         x = x.permute(0, 3, 1, 2)  # [batch_size, n_c, n_h, n_w]
         if self.shift_size > 0:
             x = torch.roll(x, shifts=(-self.shift_size, -self.shift_size), dims=(2, 3))
+            
         # [batch_size, n_c, n_h, n_w] =>  [n_windows * batch_size, n_c, window_size, window_size]
         x_windows = window_partition(x, self.window_size)
         attn_windows = self.attn(x_windows, self.attn_mask)
@@ -312,9 +279,9 @@ class SwinTransformerBlock(nn.Module):
         if self.shift_size > 0:
             x = torch.roll(x, shifts=(self.shift_size, self.shift_size), dims=(2, 3))
 
-        x = short_cut + x  # 残差
+        x = short_cut + x  # first residual
         x = x.permute(0, 2, 3, 1)  # [batch_size, n_h, n_w, n_c]
-        x = x + self.mlp(self.norm_1(x))  # 第二个残差
+        x = x + self.mlp(self.norm_1(x))  # sencond residual
         x = x.permute(0, 3, 1, 2)  # [batch_size, n_c, n_h, n_w]
 
         return x
@@ -322,7 +289,7 @@ class SwinTransformerBlock(nn.Module):
 
 class SwinTransformerBlockStack(nn.Module):
     """
-    将一个非shift的SwinTransformerBlock和一个shift的SwinTransformerBlock堆叠在一起
+    Stack a non-shift SwinTransformerBlock with a shift SwinTransformerBlock.
     """
 
     def __init__(self, n_channels, n_heads, img_size,
@@ -350,7 +317,7 @@ class SwinTransformer_emb(nn.Module):
         self.n_layers = len(n_swin_blocks)
         self.embed_dim = embed_dim
         self.patch_norm = patch_norm
-        self.num_features = int(embed_dim * 2 ** (self.n_layers - 1))  # 输出的n_channels, 除第一层外, 每层网络将n_channels翻一倍
+        self.num_features = int(embed_dim * 2 ** (self.n_layers - 1))
         self.mlp_ratio = mlp_ratio
 
         self.patch_embed = PatchEmbed(patch_size, n_channels, embed_dim, patch_norm)
@@ -388,7 +355,7 @@ class SwinTransformer_emb(nn.Module):
 
     def _init_weights(self, m):
         """
-        参照源码进行初始化
+        Initialize weights.
         """
         if isinstance(m, nn.Linear):
             trunc_normal_(m.weight, std=.02)
@@ -430,7 +397,6 @@ class SwinTransformer_coder(nn.Module):
                     qkv_bias=True, drop_rate=0., attn_drop_rate=0.,
                     patch_norm=True, n_swin_blocks=(2, 2), n_attn_heads=(2, 4))
         
-        
         # decoder
         self.decoder = nn.Sequential(
             nn.Linear(n_z, 128),
@@ -451,11 +417,9 @@ class SwinTransformer_coder(nn.Module):
     def forward(self, x):
 
         # encoder
-       
         hidden = self.encoder(x)
 
         # decoder
-        
         x_bar = self.decoder(hidden)
 
         return x_bar, hidden
@@ -516,6 +480,7 @@ class SwinTransformer(nn.Module):
             n_dec_3=n_dec_3,
             n_input=n_input,
             n_z=n_z)
+                     
         # cluster layer
         self.cluster_layer = Parameter(torch.Tensor(10, n_z))
         torch.nn.init.xavier_normal_(self.cluster_layer.data)
@@ -523,16 +488,18 @@ class SwinTransformer(nn.Module):
     def pretrain(self, path=''):
         if path == '':
             pretrain_swintae(self.cae)
+            
         # load pretrain weights
         self.cae.load_state_dict(torch.load(self.pretrain_path))
         print('load pretrained cae from', path)
 
     def forward(self, x):
-
         x_bar, z = self.cae(x)
+        
         # cluster
         q = 1.0 / (1.0 + torch.sum(
             torch.pow(z.unsqueeze(1) - self.cluster_layer, 2), 2) / self.alpha)
         q = q.pow((self.alpha + 1.0) / 2.0)
         q = (q.t() / torch.sum(q, 1)).t()
+        
         return x_bar, q
