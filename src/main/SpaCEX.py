840a2d492699b001c0c3b46baab5e93d33f89f09
Win <121435520+zipging@users.noreply.github.com>
2023-08-25 18:03:19 +0800

    Update SpaCEX.py

--- src/main/SpaCEX.py
+++ src/main/SpaCEX.py
@@ -1,38 +1,26 @@
 import torch
-import driver
-from dataset import MnistDataset
-from evalution import tsne_print
-from DEC import DEC
-from _config import Config
+from SpaCEX.src.main import driver
+from SpaCEX.src.main.clustering.DEC import DEC
+from SpaCEX.src.main._config import Config
 import pandas as pd
 import numpy as np
 import json
 import os
 
-config = Config(dataset='MNIST', model='MAE').get_parameters()
-num = 18000
+class SpaCEX():
+  def __init__(self, dataset, total):
+    super(SpaCEX, self).__init__()
+    super(SpaCEX, self).__init__()
+    self.config = Config(dataset='Gene image', model='MAE').get_parameters()
+    self.dataset = dataset
+    self.total = total
+  def forward(self):
+    cuda = torch.cuda.is_available()
+    print("use cuda: {}".format(cuda))
+    device = torch.device("cuda" if cuda else "cpu")
+    model = driver.model('Gene image', 'MAE', self.config)
+    model.to(device)
+    model.pretrain(self.dataset, batch_size=self.config['batch_size'], lr=self.config['lr'])
+    y_pred, embedding= DEC(model, self.dataset, self.total, self.config)
 
-cuda = torch.cuda.is_available()
-print("use cuda: {}".format(cuda))
-device = torch.device("cuda" if cuda else "cpu")
-dataset = MnistDataset(num)
-
-model = driver.model('MNIST', 'MAE', config)
-model.to(device)
-model.pretrain(dataset, batch_size=config['batch_size'], lr=config['lr'])
-
-y_pred, acc_train, ari_train, nmi_train = DEC(model, dataset, config)
-
-eval = np.array([acc_train, ari_train, nmi_train])
-eval = eval.T
-eval = pd.DataFrame(eval, columns = ['acc', 'ari', 'nmi'])
-
-# creat log folder
-path = "log/acc{}".format(eval['acc'].max())
-if not os.path.exists(path):
-    os.mkdir(path)
-tsne_print(model, dataset, num, path)
-
-eval.to_csv('{}eval.txt'.format(path),sep=' ')
-with open('{}eval.txt'.format(path), 'a', encoding='utf-8') as f:
-    f.write(json.dumps(config))
+    return model, y_pred, embedding
