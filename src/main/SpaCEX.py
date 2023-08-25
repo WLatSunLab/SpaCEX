973a187c7a7f2d065240ab8bd3edcf64b0c0d9e8
Win <121435520+zipging@users.noreply.github.com>
2023-08-25 18:22:46 +0800

    Update SpaCEX.py

--- src/main/SpaCEX.py
+++ src/main/SpaCEX.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from SpaCEX.src.main import driver
 from SpaCEX.src.main.clustering.DEC import DEC
 from SpaCEX.src.main._config import Config
@@ -7,20 +8,28 @@ import numpy as np
 import json
 import os
 
-class SpaCEX():
-  def __init__(self, dataset, total):
-    super(SpaCEX, self).__init__()
-    super(SpaCEX, self).__init__()
-    self.config = Config(dataset='Gene image', model='MAE').get_parameters()
-    self.dataset = dataset
-    self.total = total
-  def forward(self):
-    cuda = torch.cuda.is_available()
-    print("use cuda: {}".format(cuda))
-    device = torch.device("cuda" if cuda else "cpu")
-    model = driver.model('Gene image', 'MAE', self.config)
-    model.to(device)
-    model.pretrain(self.dataset, batch_size=self.config['batch_size'], lr=self.config['lr'])
-    y_pred, embedding= DEC(model, self.dataset, self.total, self.config)
 
-    return model, y_pred, embedding
+class SpaCEX():
+    def train(dataset, total):
+        config = Config(dataset='Gene image', model='MAE').get_parameters()
+        cuda = torch.cuda.is_available()
+        print("use cuda: {}".format(cuda))
+        device = torch.device("cuda" if cuda else "cpu")
+        model = driver.model('Gene image', 'MAE', config)
+        model.to(device)
+        model.pretrain(dataset, batch_size=config['batch_size'], lr=config['lr'])
+        y_pred, embedding= DEC(model, dataset, total, config)
+
+        return model, y_pred, embedding
+
+#def SpaCEX(dataset, total):
+   # config = Config(dataset='Gene image', model='MAE').get_parameters()
+   # cuda = torch.cuda.is_available()
+   # print("use cuda: {}".format(cuda))
+   # device = torch.device("cuda" if cuda else "cpu")
+   # model = driver.model('Gene image', 'MAE', config)
+   # model.to(device)
+   # model.pretrain(dataset, batch_size=config['batch_size'], lr=config['lr'])
+   # y_pred, embedding= DEC(model, dataset, total, config)
+
+   # return model, y_pred, embedding
