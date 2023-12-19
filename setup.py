d86076833ddebfdfd732fdcf1e7efa8e8fccf244
Win <121435520+zipging@users.noreply.github.com>
2023-12-19 15:19:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 
-with open("README.rst", "r", encoding="utf-8") as f:
-    __long_description__ = f.read()
   
 setup(
     name='Wenlin Li',
@@ -9,7 +7,6 @@ setup(
     packages=find_packages(),
     install_requires=[
     ],
-    packages = ["SpaCEX"],
     description='Gene embeddings geneartion, Enhancement of the transcriptomic coverage, SVG detection, Spatial clustering',
     author='Wenlin Li',
     author_email='zipging@gmail.com',
